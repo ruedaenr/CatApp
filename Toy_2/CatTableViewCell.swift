9fbe40586ce7d46b60aea51af08055937e93a166
Enrique Rueda <ruedaenr@enriques-mbp.attlocal.net>
2019-01-07 17:43:18 -0600

    Completed 6.1

---
+++ Toy_2/CatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatTableViewCell.swift
+//  Toy_2
+//
+//  Created by Enrique Rueda on 1/7/19.
+//  Copyright © 2019 appdev. All rights reserved.
+//
+
+import UIKit
+
+class CatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
