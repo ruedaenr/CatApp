b2edbac98dd3d7e94725e58fb852f0922baed22d
Enrique Rueda <ruedaenr@enriques-mbp.attlocal.net>
2019-01-07 15:27:39 -0600

    First Commit

---
+++ Toy_2/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Toy_2
+//
+//  Created by Enrique Rueda on 1/7/19.
+//  Copyright © 2019 appdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var NameTextField: UITextField!
+    
+    @IBOutlet weak var ClassTextField: UITextField!
+    
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ Toy_2Tests/Toy_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Toy_2Tests.swift
+//  Toy_2Tests
+//
+//  Created by Enrique Rueda on 1/7/19.
+//  Copyright © 2019 appdev. All rights reserved.
+//
+
+import XCTest
+@testable import Toy_2
+
+class Toy_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
