4d602566697004bf1ac8947fcf29540b1c237c78
Enrique Rueda <ruedaenr@enriques-mbp.attlocal.net>
2019-01-07 16:39:26 -0600

    Thread 1 Error

--- Toy_2/ViewController.swift
+++ Toy_2/ViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var submitted : Bool = false
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    @IBOutlet weak var ClassLabel: UILabel!
+    
     @IBOutlet weak var NameTextField: UITextField!
     
     @IBOutlet weak var ClassTextField: UITextField!
     
     @IBOutlet weak var SubmitButton: UIButton!
     
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
     @IBAction func SubmitButtonTapped(_ sender: Any) {
+        if(submitted){
+            NameLabel.isHidden = false
+            ClassLabel.isHidden = false
+            NameTextField.isHidden = false
+            ClassTextField.isHidden = false
+            DisplayLabel.isHidden = true
+            SubmitButton.setTitle("Submit", for: .normal)
+        } else{
+            NameLabel.isHidden = true
+            ClassLabel.isHidden = true
+            NameTextField.isHidden = true
+            ClassTextField.isHidden = true
+            let name = NameTextField.text ?? ""
+            let classYr = ClassTextField.text ?? ""
+            DisplayLabel.text = "Welcome, " + name + " of " + classYr + "!"
+            NameTextField.text = ""
+            ClassTextField.text = ""
+            DisplayLabel.isHidden = false
+            SubmitButton.setTitle("Clear", for: .normal)
+        }
+        
     }
     
     override func viewDidLoad() {
